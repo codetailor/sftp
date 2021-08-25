aa33ce185b9896f93a43f869307f79e2205b816e
Ricardo Nunes <ricardonunesdev@gmail.com>
2021-08-25 19:05:01 +0100

    Added test to download a file

--- test/index.js
+++ test/index.js
@@ -3,11 +3,26 @@
 const test = require('tape');
 const sftp = require('../index');
 
+const fs = require('fs');
+const path = require('path');
+
 // Source: https://www.sftp.net/public-online-sftp-servers
+const FTP_HOST = 'test.rebex.net';
+const FTP_PORT = 22;
+const FTP_USER = 'demo';
+const FTP_PASS = 'password';
+const FTP_TEST_FILE = 'readme.txt';
 
 test('Connect to a free SFTP server', async (t) => {
-	let conn = await sftp.connect('test.rebex.net', 22, 'demo', 'password');
+	const conn = await sftp.connect(FTP_HOST, FTP_PORT, FTP_USER, FTP_PASS);
 	t.ok(conn);
 	sftp.disconnect();
 });
 
+test('Download a file from a free SFTP server', async (t) => {
+	const conn = await sftp.connect(FTP_HOST, FTP_PORT, FTP_USER, FTP_PASS);
+	const res = await sftp.downloadFile(conn, FTP_TEST_FILE, path.join('test', FTP_TEST_FILE));
+	t.ok(res);
+	sftp.disconnect();
+	fs.unlinkSync(path.join('test', FTP_TEST_FILE));
+});
