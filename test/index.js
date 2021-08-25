0ada81f3f38d0981205d829798ac87abb647cf91
Ricardo Nunes <ricardonunesdev@gmail.com>
2021-08-25 19:13:05 +0100

    Added comments to "download file" test

--- test/index.js
+++ test/index.js
@@ -14,16 +14,26 @@ const FTP_PASS = 'password';
 const FTP_TEST_FILE = 'readme.txt';
 
 test('Connect to a free SFTP server', async (t) => {
+	// Init
 	const conn = await sftp.connect(FTP_HOST, FTP_PORT, FTP_USER, FTP_PASS);
+
+	// Check
 	t.ok(conn);
+
+	// Cleanup
 	sftp.disconnect();
 });
 
 test('Download a file from a free SFTP server', async (t) => {
+	// Init
 	const conn = await sftp.connect(FTP_HOST, FTP_PORT, FTP_USER, FTP_PASS);
 	const localFilePath = path.join('test', FTP_TEST_FILE);
 	const res = await sftp.downloadFile(conn, FTP_TEST_FILE, localFilePath);
+
+	// Check
 	t.ok(res);
+
+	// Cleanup
 	sftp.disconnect();
 	fs.unlinkSync(localFilePath);
 });
